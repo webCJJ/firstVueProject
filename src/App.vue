f59cbc219d17a16de1b566847f0649b7b4e62e9f
cjj <1228424102@qq.com>
2017-09-08 16:02:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+    <detailComponent></detailComponent>
+    <listComponent></listComponent>
+  </div>
+</template>
+
+<script>
+import listComponent from "./components/list-component.vue"
+import detailComponent from "./components/detail-component.vue"
+export default {
+  name: 'app',
+  components:{listComponent,detailComponent}
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/list-component.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id='listComponent'>
+       <ul v-for="(item,index) in items">
+             <li>{{item.someWhere}}</li>
+       </ul>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+       name:'lists',
+        data(){
+            return{
+               items:[
+               {msg:"中国",someWhere:"上海"},
+               {msg:"中国",someWhere:"北京"},
+               {msg:"中国",someWhere:"哈尔滨"}
+               ]
+            }
+        },
+
+    }
+</script>
+<style>
+  li{list-style:none}
+</style>
